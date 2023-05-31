cb4b58df637dee20fa0c6b9c6e827e85c1f989ce
juan <juangui097@gmail.com>
2023-05-31 00:11:03 -0500

    Function

---
+++ script.js
@@ -0,0 +1,16 @@
+const searchInsert = function(nums, target) {
+  let indexForNum = 0;
+  let exists = nums.indexOf(target);
+  if (exists !== -1) {
+    return exists;
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > target) {
+      indexForNum = nums.indexOf(nums[i]);
+      return indexForNum;
+    } else {
+      indexForNum++;
+    }
+  }
+  return indexForNum;
+};
